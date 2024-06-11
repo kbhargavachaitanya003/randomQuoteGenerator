bcc227fc7fa192e5f8f42e570ff65b5263a19a35
“CS21B029” <cs21b029@iittp.ac.in>
2024-06-11 12:26:16 +0530

    first commit

--- src/Components/QuoteDisplay.tsx
+++ src/Components/QuoteDisplay.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import useQuoteStore from '../quoteStore';
+import useQuoteStore from '../Store/quoteStore';
 import { Box, Button, Typography } from '@mui/material';
 import '../Styles/QuoteDisplay.css';
 
